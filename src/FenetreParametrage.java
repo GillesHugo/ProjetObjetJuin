4bc83f0940432d7d55487c16536b69102667625d
Manon <manon.cvre@gmail.com>
2023-06-02 11:00:49 +0200

    initalisation fenetre paramètres

---
+++ src/FenetreParametrage.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FenetreParametrage extends JFrame implements ActionListener {
+
+    private void build(){
+
+    }
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
